e9cb57624ce9e436942d6f4c99f2855d0d08d55a
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-01-22 22:26:15 -0500

    blockchain can now creat new blocks, transactions and mine new blocks

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
+  this.pendingTransactions = [];
 }
 
 //nocne is pretty much a proof of work number
@@ -8,17 +8,34 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timeStamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash
   };
 
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock);
 
   return newBlock;
 }
+
+Blockchain.prototype.getLastBlock = function() {
+  return this.chain[this.chain.length-1]
+}
+
+Blockchain.prototype.createNewtransaction = function(amount, sender, recipient) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient
+  }
+
+  this.pendingTransactions.push(newTransaction)
+
+  return this.getLastBlock()['index'] + 1
+}
+
 
 
 module.exports = Blockchain
--- dev/test.js
+++ dev/test.js
@@ -1,5 +1,10 @@
 const Blockchain = require('./blockchain')
 
 const bitcoin = new Blockchain();
+bitcoin.createNewBlock(7234234, '8908SDF09SD8F', 'ASDAS90A6787')
 
-console.log(bitcoin)
+bitcoin.createNewtransaction(100, 'Alex8sada980123hjk', 'PemaPdasdhhj2')
+
+bitcoin.createNewBlock(12312, '234234KJJ', 'OGHB21sds909')
+
+console.log(bitcoin.chain[1])
